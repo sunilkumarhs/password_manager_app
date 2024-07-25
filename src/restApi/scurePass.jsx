d1ef2d59ff866c6b8f6e34f668034f6e3d6542e2
sunilkumarhs <sunilkumarhs974117@gmail.com>
2024-07-25 17:06:09 +0530

    small push

--- src/restApi/scurePass.jsx
+++ src/restApi/scurePass.jsx
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const api = axios.create({
   // eslint-disable-next-line no-undef
-  // baseURL: process.env.SECUREPASS_URL,
-  baseURL: "http://localhost:5000",
+  baseURL: process.env.SECUREPASS_URL,
+  // baseURL: "http://localhost:5000",
 });
